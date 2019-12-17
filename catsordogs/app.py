2f68d7a03e33205be092a0a0addc799dc69b02ce
Daniel Estrela <destrela@gmail.com>
2019-12-17 00:29:27 -0300

    Change the names of the form fields Add persistency with mysql

--- catsordogs/app.py
+++ catsordogs/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+import mysql.connector
 
 app = Flask("Cats or Dogs")
+
+insert_clause = "INSERT INTO catsordogs (name, color, pet) VALUES (%s, %s, %s)"
 
 @app.route('/')
 def index():
@@ -8,10 +11,14 @@ def index():
 
 @app.route('/', methods=['POST'])
 def insertData():
-    userName = request.form['userName']
-    favoriteColor = request.form['favoriteColor']
-    catsOrDogs = request.form['catsOrDogs']
-    return render_template('insert_response.html', userName=userName, favoriteColor=favoriteColor, catsOrDogs=catsOrDogs)
+    form_data = (request.form['name'], request.form['color'], request.form['pet'])
+    con = mysql.connector.connect(user='daniel', password='Helo#2009#Isa', host='127.0.0.1', database='catsordogs')
+    cursor = con.cursor()
+    cursor.execute(insert_clause, form_data)
+    con.commit()
+    cursor.close()
+    con.close()
+    return render_template('insert_response.html', name=form_data[0], color=form_data[1], pet=form_data[2])
 
 
 if __name__ == '__main__':
